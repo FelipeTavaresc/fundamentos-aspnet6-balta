8356b35ac2918621b6a79a5e5329e348f9117ba6
Felipe Tavares <felipe.tavares@outlook.com>
2024-08-07 19:18:57 -0300

    Minimal APIs post

--- modulo1/MyAPI/Program.cs
+++ modulo1/MyAPI/Program.cs
@@ -1,7 +1,20 @@
+using MyAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/test", () => "Testing!");
+app.MapGet("/", () => {
+    return Results.Ok("Hello World");
+});
+
+app.MapGet("/{nome}", (string nome) => {
+    return Results.Ok($"Hello {nome}");
+});
+
+app.MapPost("/", (User user) => {
+    return Results.Ok(user);
+} );
 
 app.Run();
+
+
---
+++ modulo1/MyAPI/Properties/Models/User.cs
@@ -0,0 +1,6 @@
+namespace MyAPI.Models{
+    public class User{
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
