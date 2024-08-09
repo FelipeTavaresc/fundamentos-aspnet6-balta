8a1aa218618b3217e25ad36464545aea84324258
Felipe Tavares <felipe.tavares@outlook.com>
2024-08-09 08:24:34 -0300

    Add HomeController e configurando rotas

---
+++ modulo2/Todo/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/")]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
