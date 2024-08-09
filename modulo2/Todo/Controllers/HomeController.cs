a1a693bdcf9317bec4a222fc4ce3ff159ab83cff
Felipe Tavares <felipe.tavares@outlook.com>
2024-08-09 20:15:11 -0300

    Obtendo itens do banco de dados

--- modulo2/Todo/Controllers/HomeController.cs
+++ modulo2/Todo/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+using Todo.Models;
 
 namespace Todo.Controllers
 {
@@ -7,9 +9,9 @@ namespace Todo.Controllers
     {
         [HttpGet]
         [Route("/")]
-        public string Get()
+        public List<TodoModel> Get([FromServices] AppDbContext context)
         {
-            return "Hello World";
+            return context.Todos.ToList();
         }
     }
 }
