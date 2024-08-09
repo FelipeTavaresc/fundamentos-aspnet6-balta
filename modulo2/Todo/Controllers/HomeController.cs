0e4d2248be0c0524a3d3955827b693c3b8d22a5f
Felipe Tavares <felipe.tavares@outlook.com>
2024-08-09 20:33:43 -0300

    Atualizando e excluindo registros

--- modulo2/Todo/Controllers/HomeController.cs
+++ modulo2/Todo/Controllers/HomeController.cs
@@ -14,11 +14,44 @@ namespace Todo.Controllers
             return context.Todos.ToList();
         }
 
+        [HttpGet("/{id:int}")]
+        public TodoModel GetById([FromRoute] int id, [FromServices] AppDbContext context)
+        {
+            return context.Todos.FirstOrDefault(x => x.Id == id) ?? new TodoModel();
+        }
+
         [HttpPost]
         [Route("/")]
         public TodoModel Post([FromBody] TodoModel model, [FromServices] AppDbContext context)
         {
             context.Todos.Add(model);
+            context.SaveChanges();
+            return model;
+        }
+
+        [HttpPut]
+        [Route("/{id:int}")]
+        public TodoModel Put([FromRoute] int id, [FromBody] TodoModel todo, [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return todo;
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+            return model;
+        }
+
+        [HttpDelete]
+        [Route("/{id:int}")]
+        public TodoModel Delete([FromRoute] int id, [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+
+            context.Todos.Remove(model);
             context.SaveChanges();
             return model;
         }
