a440f0eead3a1828733ccf7497a2559117a2b1e7
Felipe Tavares <felipe.tavares@outlook.com>
2024-08-14 08:35:05 -0300

    Criando o banco de dados

--- modulo3/Blog/Data/BlogDataContext.cs
+++ modulo3/Blog/Data/BlogDataContext.cs
@@ -14,7 +14,7 @@ namespace Blog.Data
         public DbSet<PostWithTagsCount> PostWithTagsCounts { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer("Server=localhost,1433;Database=BlogEF;User ID=sa;Password=1q2w3e4r@#$");
+            => options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$; TrustServerCertificate=Yes");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
