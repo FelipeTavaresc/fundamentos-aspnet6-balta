fcc7a86720d8000a692addc749991a7aaf7d24dd
Felipe Tavares <felipe.tavares@outlook.com>
2024-08-15 08:13:46 -0300

    Nomeando endpoint

--- modulo3/Blog/Models/Category.cs
+++ modulo3/Blog/Models/Category.cs
@@ -1,17 +1,13 @@
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Blog.Models
 {
     public class Category
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string Slug { get; set; }
+        public string? Slug { get; set; }
 
-        public List<Post> Posts { get; set; }
+        public List<Post> Posts { get; set; } = new();
     }
 }
