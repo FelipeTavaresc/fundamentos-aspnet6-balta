bdf6652324c2c3798887843d6bef3b44022590f4
Felipe Tavares <felipe.tavares@outlook.com>
2024-08-21 05:53:10 -0300

    Classe para padronização de retornos

---
+++ modulo3/Blog/ViewModels/ResultViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Blog.ViewModels
+{
+    public class ResultViewModel<T>
+    {
+        public ResultViewModel(T? data, List<string> erros)
+        {
+            Data = data;
+            Erros = erros;
+        }
+
+        public ResultViewModel(T? data) => Data = data;
+
+        public ResultViewModel(List<string> erros) => Erros = erros;
+
+        public ResultViewModel(string error) => Erros.Add(error);
+
+        public T? Data { get; private set; }
+        public List<string> Erros { get; private set; } = new();
+    }
+}
