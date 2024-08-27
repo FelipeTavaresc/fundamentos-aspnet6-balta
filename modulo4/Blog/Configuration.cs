8c9a872cf5fa9e888def990ecc2fcbf1f8493b2d
Felipe Tavares <felipe.tavares@outlook.com>
2024-08-27 19:57:29 -0300

    iniciando modulo 4

---
+++ modulo4/Blog/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog
+{
+    public static class Configuration
+    {
+        public static string JwtKey { get; set; } = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
+    }
+}
---
+++ modulo4/Blog/Models/Category.cs
@@ -0,0 +1,13 @@
+namespace Blog.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Slug { get; set; }
+
+        public List<Post> Posts { get; set; } = new();
+    }
+}
