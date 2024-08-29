ad13ffb91e407fe5ecf167ea94358308eba8ea33
Felipe Tavares <felipe.tavares@outlook.com>
2024-08-29 05:32:16 -0300

    Configurando autenticação e autorização

--- modulo4/Blog/Program.cs
+++ modulo4/Blog/Program.cs
@@ -1,7 +1,27 @@
+using Blog;
 using Blog.Data;
 using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder.Services
     .AddControllers()
@@ -14,6 +34,10 @@ builder.Services.AddTransient<TokenService>();
 //builder.Services.AddSingleton()  Uma instância para toda aplicação
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
