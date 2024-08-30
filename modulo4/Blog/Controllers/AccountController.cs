33ac7e5df70902c267a27c18c09e92e7dffb0dae
Felipe Tavares <felipe.tavares@outlook.com>
2024-08-30 06:00:25 -0300

    Autenticando o usuário

--- modulo4/Blog/Controllers/AccountController.cs
+++ modulo4/Blog/Controllers/AccountController.cs
@@ -61,12 +61,35 @@ namespace Blog.Controllers
             
         }
 
-        [HttpPost("v1/login")]
-        public IActionResult Login()
+        [HttpPost("v1/accounts/login")]
+        public async Task<IActionResult> Login([FromBody]LoginViewModel model,
+                                   [FromServices] BlogDataContext context)
         {
-            var token = _tokenService.GenerateToken(null);
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErros()));
 
-            return Ok(token);
+            var user = await context
+                .Users
+                .AsNoTracking()
+                .Include(x => x.Roles)
+                .FirstOrDefaultAsync(x => x.Email == model.Email);
+
+            if (user == null)
+                return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
+
+            if (!PasswordHasher.Verify(user.PasswordHash, model.Password))
+                return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
+
+            try
+            {
+                var token = _tokenService.GenerateToken(user);
+                return Ok(new ResultViewModel<string>(token, null));
+            }
+            catch 
+            {
+                return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
+            }
+            
         }
 
 
