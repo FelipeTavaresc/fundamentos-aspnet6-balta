43a3864a86edf0a599ba82a0425d45ca6312651a
Felipe Tavares <felipe.tavares@outlook.com>
2024-09-04 05:31:07 -0300

    inicio modulo 5

---
+++ modulo5/Blog/Models/Role.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Blog.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+
+        public IList<User> Users { get; set; }
+    }
+}
---
+++ modulo5/Blog/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o Email")]
+        [EmailAddress(ErrorMessage = "O Email é inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a Senha")]
+        public string Password { get; set; }
+    }
+}
