1024a8ced1e700c0b8d10770f7b67ede128dbff2
Felipe Tavares <felipe.tavares@outlook.com>
2024-09-09 05:36:07 -0300

    Permitindo arquivos estátticos e realizando upload de imagens para o diretório wwwroot

--- modulo5/Blog/Controllers/AccountController.cs
+++ modulo5/Blog/Controllers/AccountController.cs
@@ -3,10 +3,12 @@ using Blog.Extensions;
 using Blog.Models;
 using Blog.Services;
 using Blog.ViewModels;
+using Blog.ViewModels.Accounts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SecureIdentity.Password;
+using System.Text.RegularExpressions;
 
 namespace Blog.Controllers
 {
@@ -52,7 +54,8 @@ namespace Blog.Controllers
 
                 return Ok(new ResultViewModel<dynamic>(new
                 {
-                    user = user.Email,password
+                    user = user.Email,
+                    password
                 }));
             }
             catch (DbUpdateException)
@@ -63,11 +66,11 @@ namespace Blog.Controllers
             {
                 return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
             }
-            
+
         }
 
         [HttpPost("v1/accounts/login")]
-        public async Task<IActionResult> Login([FromBody]LoginViewModel model,
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model,
                                    [FromServices] BlogDataContext context)
         {
             if (!ModelState.IsValid)
@@ -90,13 +93,51 @@ namespace Blog.Controllers
                 var token = _tokenService.GenerateToken(user);
                 return Ok(new ResultViewModel<string>(token, null));
             }
-            catch 
+            catch
             {
                 return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
             }
-            
+
         }
 
+        [Authorize]
+        [HttpPost("v1/accounts/upload-image")]
+        public async Task<IActionResult> UploadImage([FromBody] UploadImageViewModel model,
+                                                     [FromServices] BlogDataContext context)
+        {
+            var fileName = $"{Guid.NewGuid().ToString()}.jpg";
+            var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(model.Base64Image, "");
+            var bytes = Convert.FromBase64String(data);
 
+            try
+            {
+                await System.IO.File.WriteAllBytesAsync($"wwwroot/images/{fileName}", bytes);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new ResultViewModel<string>("05x04 - Falha interna no servidor"));
+            }
+
+            var user = await context
+                             .Users
+                             .FirstOrDefaultAsync(x => x.Email == User.Identity.Name);
+
+            if (user == null)
+                return NotFound(new ResultViewModel<User>("Usuário não encontrado"));
+
+            user.Image = $"https://localhost:7118/images/{fileName}";
+
+            try
+            {
+                context.Users.Update(user);
+                await context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new ResultViewModel<string>("05x04 - Falha interna no servidor"));
+            }
+
+            return Ok(new ResultViewModel<string>("Imagem alterada com sucesso!", null));
+        }
     }
 }
