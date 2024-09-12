06d31198d1ef8ac76383dbb13c94b68152e10359
Felipe Tavares <felipe.tavares@outlook.com>
2024-09-12 05:26:10 -0300

    Filtrando posts por categoria

--- modulo5/Blog/Controllers/PostController.cs
+++ modulo5/Blog/Controllers/PostController.cs
@@ -50,7 +50,76 @@ namespace Blog.Controllers
             }
             catch (Exception)
             {
-                return StatusCode(500, new ResultViewModel<List<Category>>("05X04 - Falha interna no servidor"))
+                return StatusCode(500, new ResultViewModel<List<Category>>("05X04 - Falha interna no servidor"));
+            }
+        }
+
+        [HttpGet("v1/posts/{id:int}")]
+        public async Task<IActionResult> DetailsAsync(
+            [FromServices] BlogDataContext context,
+            [FromRoute] int id)
+        {
+            try
+            {
+                var post = await context
+                    .Posts
+                    .AsNoTracking()
+                    .Include(x => x.Author)
+                    .ThenInclude(x => x.Roles)
+                    .Include(x => x.Category)
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                if (post == null)
+                    return NotFound(new ResultViewModel<Post>("Conteúdo não encontrado"));
+
+                return Ok(new ResultViewModel<Post>(post));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new ResultViewModel<List<Category>>("05X04 - Falha interna no servidor"));
+            }
+        }
+
+        [HttpGet("v1/posts/category/{category}")]
+        public async Task<IActionResult> GetByCategoryAsync(
+        [FromRoute] string category,
+        [FromServices] BlogDataContext context,
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 25)
+        {
+            try
+            {
+                var count = await context.Posts.AsNoTracking().CountAsync();
+                var posts = await context
+                    .Posts
+                    .AsNoTracking()
+                    .Include(x => x.Author)
+                    .Include(x => x.Category)
+                    .Where(x => x.Category.Slug == category)
+                    .Select(x => new ListPostsViewModel
+                    {
+                        Id = x.Id,
+                        Title = x.Title,
+                        Slug = x.Slug,
+                        LastUpdateDate = x.LastUpdateDate,
+                        Category = x.Category.Name ?? string.Empty,
+                        Author = $"{x.Author.Name} ({x.Author.Email})"
+                    })
+                    .Skip(page * pageSize)
+                    .Take(pageSize)
+                    .OrderByDescending(x => x.LastUpdateDate)
+                    .ToListAsync();
+                return Ok(new ResultViewModel<dynamic>(new
+                {
+                    total = count,
+                    page,
+                    pageSize,
+                    posts
+                }));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<List<Category>>("05X04 - Falha interna no servidor"));
             }
         }
     }
